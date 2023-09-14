616eb7b1c32c76dbcb54de391bb566be41ad7105
Vialoha <vi62469@gmail.com>
2023-09-14 17:36:09 +0100

    ok

--- routes/projects.routes.js
+++ routes/projects.routes.js
@@ -11,6 +11,8 @@ const {isAuthenticated} = require("../middleware/jwt.middleware");
 
 const fileUploader = require('../config/cloudinary.config');
 
+const cloudinary = require("cloudinary").v2;
+
 //  POST /api/projects  -  Creates a new project
 
 router.post("/projects", isAuthenticated, fileUploader.array('images',10), async (req, res, next) => {
@@ -83,18 +85,51 @@ router.get("/projects/:projectId", (req, res, next) => {
 
 
 // PUT  /api/projects/:projectId  -  Updates a specific project by id
-router.put("/projects/:projectId", (req, res, next) => {
+router.put("/projects/:projectId", fileUploader.array("images", 10), async (req, res, next) => {
+  try {
     const { projectId } = req.params;
-  
-    if (!mongoose.Types.ObjectId.isValid(projectId)) {
-      res.status(400).json({ message: "Specified id is not valid" });
-      return;
+    const { title, description, tags, sociallinksproject, creationdate, isPrivate } = req.body;
+    const newImageUrls = [];
+
+    // Upload and get URLs for the newly added images
+    for (const file of req.files) {
+      const result = await cloudinary.uploader.upload(file.path, {
+        folder: "test", // Optional: specify a folder in Cloudinary
+      });
+      newImageUrls.push(result.secure_url); // Push the Cloudinary image URL
     }
-  
-    Project.findByIdAndUpdate(projectId, req.body, { new: true })
-      .then((updatedProject) => res.json(updatedProject))
-      .catch((error) => res.json(error));
-  });
+
+    // Find the project by ID to get existing image URLs
+    const existingProject = await Project.findById(projectId);
+
+    if (!existingProject) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    // Concatenate existing and new image URLs
+    const updatedImageUrls = [...existingProject.images, ...newImageUrls];
+
+    // Find the project by ID and update it with the merged image URLs
+    const updatedProject = await Project.findByIdAndUpdate(
+      projectId,
+      {
+        title,
+        description,
+        tags,
+        images: updatedImageUrls, // Save the merged image URLs
+        sociallinksproject,
+        creationdate,
+        isPrivate,
+      },
+      { new: true }
+    );
+
+    res.status(200).json(updatedProject);
+  } catch (error) {
+    console.error("Error updating project:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
   
 

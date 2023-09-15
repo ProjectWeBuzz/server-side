2a288c0d2b9ea33393e29e28142fa2bc0b766e91
ThePainterThree <120008170+ThePainterThree@users.noreply.github.com>
2023-09-15 18:57:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
-const PORT = process.env.ORIGIN || 5005;
+const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
